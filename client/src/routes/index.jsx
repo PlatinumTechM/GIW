30b3191b678153a4677832a9d1d8395d4a4e804f
krish-kalsariya <kalsariyakrish22@gmail.com>
2026-04-08 18:21:07 +0530

    Home Page Design

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,6 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "@/App";
 import Home from "@/modules/home/Home";
+// import Home from "@/modules/home/Home1";
+// import Home from "@/modules/home/Home2";
+// import Home from "@/modules/home/Home3";
+// import Home from "@/modules/home/Home4";
+// import Home from "@/modules/home/Home5";
+// import Home from "@/modules/home/Home6";
+// import Home from "@/modules/home/Home7";
+// import Home from "@/modules/home/Home8";
+// import Home from "@/modules/home/Home9";
+// import Home from "@/modules/home/Home10";
+// import Home from "@/modules/home/Home11";
+// import Home from "@/modules/home/Home12";
+// import Home from "@/modules/home/Home13";
+// import Home from "@/modules/home/Home14";
+// import Home from "@/modules/home/Home15";
+// import Home from "@/modules/home/Home16";
+// import Home from "@/modules/home/Home17";
 import Register from "@/modules/users/Register";
 import Login from "@/modules/users/Login";
 import ForgotPassword from "@/modules/users/ForgotPassword";
@@ -12,7 +29,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home />,
+        element: <Home/>,
       },
       {
         path: "/register",
