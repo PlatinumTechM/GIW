dcbc5b301fe9f8c278e047ded88399b168ce0e44
Platinum Tech <platinumsoftech@gmail.com>
2026-04-10 11:43:05 +0530

    Start Backend

---
+++ server/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+// app.use("/api/v1/auth");
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    data: null,
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    data: null,
+  });
+});
+
+export default app;
